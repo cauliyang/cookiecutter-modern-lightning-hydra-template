fa1ca2273c2d4a73137a2980b7b7f6ada55326ae
Yangyang Li <yangyang.li@northwestern.edu>
2024-12-10 14:46:33 -0600

    refactor: Update import path for utility functions

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ from lightning import Callback, LightningDataModule, LightningModule, Trainer
 from lightning.pytorch.loggers import Logger
 from omegaconf import DictConfig
 
-from riker.utils import (
+from project.utils import (
     RankedLogger,
     extras,
     get_metric_value,
